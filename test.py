a19b040e5ddb865d981f835830fa32689fea3656
lewdalewda <101987412+lewdalewda@users.noreply.github.com>
2025-12-06 03:15:25 +0100

    added test.py

---
+++ test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13_V4
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in13_V4 Demo")
+    
+    epd = epd2in13_V4.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    
+    if 1:
+        logging.info("E-paper refresh")
+        epd.init()
+        logging.info("1.Drawing on the image...")
+        image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+        draw = ImageDraw.Draw(image)
+        
+        draw.text((120, 60), 'i like to suck toes', font = font15, fill = 0)
+        
+        # image = image.rotate(180) # rotate
+        epd.display(epd.getbuffer(image))
+        time.sleep(2)
+        
+       
+   
+
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13_V4.epdconfig.module_exit(cleanup=True)
+    exit()
