23b757ff30e06c374c8fbccadedbda98da535931
lewdalewda <101987412+lewdalewda@users.noreply.github.com>
2025-12-29 19:39:20 +0100

    Add files via upload

---
+++ testidk.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in13_V4
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+image = Image.open(os.path.join(picdir, 'frame1.bmp'))
+
+try:
+    logging.info("epd2in13_V4 Demo")
+    
+    epd = epd2in13_V4.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    
+    if 1:
+        logging.info("E-paper refresh")
+        epd.init()
+        logging.info("1.Drawing on the image...")
+           
+        draw = ImageDraw.Draw(image)
+        
+      
+        
+       
+        epd.display(draw)
+        time.sleep(2)
+        
+       
+   
+
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in13_V4.epdconfig.module_exit(cleanup=True)
+    exit()
